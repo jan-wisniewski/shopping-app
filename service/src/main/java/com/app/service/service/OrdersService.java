b5c970861c52e7aed2836f1dc767030644a05983
Jan Wiśniewski <44075213+jan-wisniewski@users.noreply.github.com>
2020-04-16 13:42:30 +0200

    Orders Service class completed, new methods added

--- service/src/main/java/com/app/service/service/OrdersService.java
+++ service/src/main/java/com/app/service/service/OrdersService.java
@@ -1,6 +1,7 @@
 package com.app.service.service;
 
 import com.app.service.exceptions.ServiceException;
+import org.eclipse.collections.impl.collector.Collectors2;
 import persistence.converters.impl.OrdersJsonConverter;
 import persistence.enums.Category;
 import persistence.models.Customer;
@@ -9,14 +10,20 @@ import persistence.models.Product;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class OrdersService {
 
     private List<Order> orders;
+    private static final String DISCOUNT_TWO_DAYS_TO_ORDER = "0.02";
+    private static final String DISCOUNT_CUSTOMER_UNDER_OR_EQUAL_25 = "0.03";
 
     public OrdersService(String jsonFilename) {
         this.orders = init(jsonFilename);
@@ -91,4 +98,92 @@ public class OrdersService {
                 .getKey();
     }
 
+    public Category categoryWhichProductsBuyTheMost() {
+        return orders.stream()
+                .flatMap(order -> Collections.nCopies(order.getQuantity(), order.getProduct().getCategory()).stream())
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream()
+                .max(Comparator.comparing(Map.Entry::getValue))
+                .orElseThrow(() -> new ServiceException("FAILED"))
+                .getKey();
+    }
+
+    public Map<Month, Integer> monthAndOrderedProducts() {
+        return orders.stream()
+                .collect(Collectors.groupingBy(o -> o.getOrderDate().getMonth(),
+                        Collectors.summingInt(Order::getQuantity)));
+
+    }
+
+    public Map<Month, Category> mostPopularCategoryInMonth() {
+        return orders.stream()
+                .collect(Collectors.groupingBy(o -> o.getOrderDate().getMonth()))
+                .entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, e -> e.getValue()
+                        .stream()
+                        .collect(Collectors.groupingBy(o ->
+                                o.getProduct().getCategory(), Collectors.counting()))
+                        .entrySet().stream()
+                        .max(Comparator.comparing(Map.Entry::getKey))
+                        .orElseThrow()
+                        .getKey()
+
+                ));
+    }
+
+    public List<Customer> findCustomersByNumberOfOrders(int quantity) {
+        if (quantity <= 0) {
+            throw new IllegalArgumentException("Incorrect number of oders");
+        }
+        return orders.stream()
+                .filter(o -> o.getQuantity() >= quantity)
+                .map(Order::getCustomer)
+                .collect(Collectors.toList());
+    }
+
+    public BigDecimal averagePriceInTimeRange(LocalDate from, LocalDate to) {
+        return orders.stream()
+                .filter(o -> o.getOrderDate().isAfter(from) && o.getOrderDate().isBefore(to))
+                .map(o -> (o.getProduct().getPrice().multiply(BigDecimal.valueOf(o.getQuantity()))))
+                .collect(Collectors2.summarizingBigDecimal(o -> o))
+                .getAverage();
+    }
+
+    public BigDecimal countTotalPriceWithDiscounts() {
+
+        return orders
+                .stream()
+                .map(o -> o
+                        .getProduct()
+                        .getPrice()
+                        .multiply(BigDecimal.ONE.subtract(calculateDiscount(o))
+                                .multiply(BigDecimal.valueOf(o.getQuantity()))))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    private BigDecimal calculateDiscount(Order order) {
+        if (isAtLeastTwoDaysToOrder(order)) {
+            return new BigDecimal(DISCOUNT_TWO_DAYS_TO_ORDER);
+        }
+
+        if (isCustomerUnderOrEqual25Years(order)) {
+            return new BigDecimal(DISCOUNT_CUSTOMER_UNDER_OR_EQUAL_25);
+        }
+
+        return BigDecimal.ZERO;
+    }
+
+    private boolean isAtLeastTwoDaysToOrder(Order order) {
+        if (order == null) {
+            throw new ServiceException("Order is null");
+        }
+        return ChronoUnit.DAYS.between(order.getOrderDate(), LocalDate.now()) >= 2;
+    }
+
+    private boolean isCustomerUnderOrEqual25Years(Order order) {
+        if (order == null) {
+            throw new ServiceException("Order is null");
+        }
+        return order.getCustomer().getAge() <= 25;
+    }
 }
